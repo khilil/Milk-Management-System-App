c15f42c5cc261a564faee35dfb120447d18e05e9
khilil <khililpatel0704@gmail.com>
2025-06-18 20:35:31 +0530

    Customer create problem fix (Error like Address is required)

--- database-connect/admin/customer/AddCustomer.js
+++ database-connect/admin/customer/AddCustomer.js
@@ -20,12 +20,11 @@ export const fetchAddresses = async () => {
 };
 
 
+// const API_URL = API_CONFIG.BASE_URL; // Use customer.php for addCustomer
 export const addCustomer = async (customerData) => {
   try {
-    const response = await axios.post(API_URL, customerData, {
-      headers: {
-        'Content-Type': 'application/json',
-      },
+    const response = await axios.post(API_CONFIG.BASE_URL, customerData, {
+      headers: { 'Content-Type': 'application/json' },
       timeout: 5000,
     });
     return response.data;
